72bdabd89709f0a774162d170b090db1776ed51e
mainasm <maina.sam@outlook.com>
2025-07-17 17:52:28 +0300

    django-blogsite-api-v1

---
+++ blog/serializers.py
@@ -0,0 +1,23 @@
+# Import serializers class from rest_framework
+from rest_framework import routers, serializers, viewsets
+
+# Import the models to serialize
+from .models import Post, Comment, Category
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
