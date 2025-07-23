0021f96b07c63daf4ea11f408f3da11f786f25bf
xavier4-csr <luvaidavid3@gmail.com>
2025-07-23 22:21:39 +0300

    added the profile model

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, CategoryViewSet
+from .views import PostViewSet, CommentViewSet, CategoryViewSet, TagViewSet, ProfileViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'categories', CategoryViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'profiles', ProfileViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Post, Comment, Category
-from .serializers import PostSerializer, CommentSerializer, CategorySerializer
+from .models import Post, Comment, Category, Tag, Profile
+from .serializers import PostSerializer, CommentSerializer, CategorySerializer, TagSerializer, ProfileSerializer
 
-# Create your viewsets here.
-class PostViewSet (viewsets.ModelViewSet):
+
+# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-class CommentViewSet (viewsets.ModelViewSet):
+    
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-
-class CategoryViewSet (viewsets.ModelViewSet):
+    
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    
