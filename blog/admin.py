f8638b4b66cc8eb75d70c582559c12f7820a3302
Mathenge Gitahi <mattgitahi@gmail.com>
2025-07-24 18:59:40 +0300

    adding the profile section in the admin panel with some added functionalities

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment, Category
+from .models import Post, Comment, Category, Profile  # 👈 Add Profile here
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
+admin.site.register(Profile)  # 👈 Register Profile here
