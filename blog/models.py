5d20cffd943745975a4123eeb494cd0408f6aa92
mainasm <maina.sam@outlook.com>
2025-07-31 18:58:56 +0300

    created profile section

--- blog/models.py
+++ blog/models.py
@@ -30,3 +30,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
