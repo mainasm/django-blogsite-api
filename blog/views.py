f2dc64b4d96ad487e316b955408a899cc8ab7976
AllanMutiso <alan.mutiso15@strathmore.edu>
2025-08-10 14:30:17 +0300

    updates to the assignment

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Post, Comment, Category
 from .serializers import PostSerializer, CommentSerializer, CategorySerializer
 
 # Create your viewsets here.
-class PostViewSet (viewsets.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
 
-class CommentViewSet (viewsets.ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
 
-class CategoryViewSet (viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.DjangoModelPermissions]
