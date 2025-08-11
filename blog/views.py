c75eae098f0794e5d1cc879904f245126d2555a5
mainasm <maina.sam@outlook.com>
2025-08-11 12:40:33 +0300

    added auth security enhancements

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,39 @@ from .models import Post, Comment, Category
 from .serializers import PostSerializer, CommentSerializer, CategorySerializer
 
 # Create your viewsets here.
-class PostViewSet (viewsets.ModelViewSet):
+# class PostViewSet (viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class CommentViewSet (viewsets.ModelViewSet):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+
+# class CategoryViewSet (viewsets.ModelViewSet):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+# blog/views.py
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAuthorOrReadOnly
+from .models import Post, Comment, Category
+from .serializers import PostSerializer, CommentSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    # permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
-class CommentViewSet (viewsets.ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
-class CategoryViewSet (viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # open CRUD on categories for authenticated users
