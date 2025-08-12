d382c986e89a1f8208606ec64fc8c3feaaaf58b7
Mathenge Gitahi <mattgitahi@gmail.com>
2025-08-12 15:46:01 +0300

    adding permisions for different roles

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
 from rest_framework import viewsets
 from .models import Post, Comment, Category
 from .serializers import PostSerializer, CommentSerializer, CategorySerializer
 
-# Create your viewsets here.
-class PostViewSet (viewsets.ModelViewSet):
+# Custom permission example for editor role
+class IsEditor(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.username == 'editor'
+
+class IsModerator(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.username == 'moderator'
+
+
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class CommentViewSet (viewsets.ModelViewSet):
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            # Only admin or editor can modify posts
+            return [IsAdminUser() | IsEditor()]
+        return [IsAuthenticated()]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-class CategoryViewSet (viewsets.ModelViewSet):
+    def get_permissions(self):
+        if self.action in ['destroy']:
+            # Only moderator or admin can delete comments
+            return [IsAdminUser() | IsModerator()]
+        return [IsAuthenticated()]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        # Only admin can modify categories
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
